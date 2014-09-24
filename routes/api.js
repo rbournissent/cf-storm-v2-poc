4dbdd76b6c6bd9012c89b97784cbc565da5b9eb1
leolower <Leonardo.Lower@wmg.com>
2014-09-24 17:04:08 -0300

    Token authentication.

--- routes/api.js
+++ routes/api.js
@@ -2,25 +2,32 @@ var express = require('express');
 var router = express.Router();
 var CloudFoundryApi = require('cloud-foundry');
 
-var cfConfig = require('../config/cf.json');
-
-// https://github.com/ActiveState/cloud-foundry-client-js
-
-
-var api_endpoint = cfConfig.Target;
-var api_token = cfConfig.AccessToken.replace('bearer ', '');
+var request = require('request');
+require('request').debug = true;
 
 
-var cf_api = new CloudFoundryApi(api_endpoint, {
-    token: api_token
-});
-
+function hasRequiredHeaders(req) {
+    return (!!req.headers['cf-api'] && !!req.headers['cf-token']);
+}
 
 function getOrganizations(req, res) {
+    if (!hasRequiredHeaders(req)) {
+        res.send(400, {
+            'error': 'CF headers missing'
+        });
+    }
+
+    console.log(req.headers['cf-api']);
+    console.log(req.headers['cf-token']);
+
+    var cf_api = new CloudFoundryApi(req.headers['cf-api'], {
+        token: req.headers['cf-token']
+    });
+
     cf_api.organizations.list({},
         function(err, page) {
             if (err) {
-                res.send(500, err);
+                res.send(401, {'error': 'Invalid Token'});
                 return console.log(err);
             }
 
@@ -30,10 +37,20 @@ function getOrganizations(req, res) {
 }
 
 function getApps(req, res) {
+    if (!hasRequiredHeaders(req)) {
+        res.send(400, {
+            'error': 'CF headers missing'
+        });
+    }
+
+    var cf_api = new CloudFoundryApi(req.headers['cf-api'], {
+        token: req.headers['cf-token']
+    });
+
     cf_api.apps.list({},
         function(err, page) {
             if (err) {
-                res.send(500, err);
+                res.send(401, {'error': 'Invalid Token'});
                 return console.log(err);
             }
 
@@ -43,10 +60,20 @@ function getApps(req, res) {
 }
 
 function getSpaces(req, res) {
+    if (!hasRequiredHeaders(req)) {
+        res.send(400, {
+            'error': 'CF headers missing'
+        });
+    }
+
+    var cf_api = new CloudFoundryApi(req.headers['cf-api'], {
+        token: req.headers['cf-token']
+    });
+
     cf_api.spaces.list({},
         function(err, page) {
             if (err) {
-                res.send(500, err);
+                res.send(401, {'error': 'Invalid Token'});
                 return console.log(err);
             }
 
@@ -55,9 +82,72 @@ function getSpaces(req, res) {
     );
 }
 
+function getInfo(req, res, callback) {
+    var request_options = {
+        method: 'GET',
+        url: req.headers['cf-api'] + '/v2/info',
+        json: true,
+        headers: {
+            'Accept': 'application/json'
+        }
+    };
+
+
+    request.get(request_options, function(error, response, body) {
+        if (!error) {
+            callback(error, response, body);
+        } else {
+            console.warn("request error", response.statusCode, body);
+            res.send(response.statusCode || 500, body);
+        }
+
+    });
+
+}
+
+function getToken(req, res) {
+    console.log(req.headers['cf-api']);
+    console.log(req.headers['cf-username']);
+    console.log(req.headers['cf-password']);
+    var get_info_response = getInfo(req, res, function(error, response, get_info_response) {
+        var token_endpoint = get_info_response.token_endpoint;
+
+        var req_data = {
+            "username": req.headers['cf-username'],
+            "password": req.headers['cf-password'],
+            "client_id": 'app',
+            "grant_type": 'password'
+        };
+
+        var request_options = {
+            method: 'POST',
+            url: token_endpoint + '/oauth/token',
+            form: req_data,
+            json: true,
+            headers: {
+                'Authorization': 'Basic Y2Y6',
+                'Accept': 'application/json'
+            }
+        };
+
+
+        request.post(request_options, function(error, response, body) {
+            if (!error) {
+                res.send(response.statusCode, body);
+            } else {
+                console.warn("request error", response.statusCode, body);
+                res.send(response.statusCode || 500, body);
+            }
+        });
+    });
+
+
+}
+
 router.get('/organizations', getOrganizations);
 router.get('/spaces', getSpaces);
 router.get('/apps', getApps);
+router.get('/token', getToken);
 
 
 module.exports = router;
